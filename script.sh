f7d7a5aa61b6148416e90194938bb9f45c6fcec1
Rana-Tej-Singh <17468009+Rana-Tej-Singh@users.noreply.github.com>
2020-06-28 17:13:01 +0530

    Shell Script

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -f /root/network-policy/namespace.yaml ]; then
+   kubectl create -f namespace.yaml
+else 
+  echo "File not found."
+  exit 0
+fi
+echo ""
+echo -n "Changing Namespace..."
+sleep 1
+kubectl config set-context $(kubectl config current-context) --namespace=network
+echo ""
+sleep 1 
+echo -n "Namespace changed default to network"
+echo ""
+kubectl config view --flatten --minify | grep namespace:
